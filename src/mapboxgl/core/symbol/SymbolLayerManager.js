c9216e8c2a84ecdd7e674f88203c38d87bf72d94
yangtingting@supermap.com <yangtingting@supermap.com>
2023-03-22 15:28:08 +0800

    [fix] 提交遗漏的文件
    review by huzhipeng

--- src/mapboxgl/core/symbol/SymbolLayerManager.js
+++ src/mapboxgl/core/symbol/SymbolLayerManager.js
@@ -2,13 +2,13 @@ import MapboxSymbolLayerManager from "./MapboxSymbolLayerManager";
 
 const SymbolLayerManager = () => {
     const result = {};
-    return (type) => {
+    return (type, map) => {
         if(!result[type]) {
             result[type] = {
                 mapbox: MapboxSymbolLayerManager
             }[type]
         }
-        return result[type];
+        return result[type]?.(map);
     }
 }
 export default SymbolLayerManager;
