523cb060ea7dd70ac991ac23b1ab4cf5759c9fc9
yangtingting@supermap.com <yangtingting@supermap.com>
2023-06-01 16:52:43 +0800

    [update] 优化API文档
    review by zhaoq

--- src/mapboxgl/docs/WebSymbol.js
+++ src/mapboxgl/docs/WebSymbol.js
@@ -7,17 +7,18 @@
  * @namespace
  * @category Visualization WebSymbol
  * @description 
- * **SuperMap iClient for MapboxGL 支持 Web 符号库，扩展了 [Map](https://docs.mapbox.com/mapbox-gl-js/api/map/) 的 API。**
+ * **SuperMap iClient for MapboxGL 支持 Web 符号库，扩展了 [Mapbox](https://docs.mapbox.com/mapbox-gl-js/api/) 的 API。**
  * 
  * ## 新增 API
- * ## Map.loadSymbol
+ * ## mapboxgl.Map.prototype.loadSymbol
  * 加载预定义符号。
  * 
  * 参数名称			     |类型			 |描述  
  * :----				|:---		    |:---	
  * id				    |string		    |[预定义符号ID]()
- * callback			    |function		|在符号加载完成后调用，如果有错误，则带错误参数
+ * callback			    |function		|在符号加载完成后调用，返回符号信息；如果有错误，则返回错误参数。
  * 
+ * **Example**
  * ```
  * map.loadSymbol('point-1', (error, symbol) => {
  *       if (error) throw error;
@@ -27,93 +28,85 @@
  * ```
  * 
  * 
- * ## Map.addSymbol
- * 在样式中添加一个符号。该符号可以显示在地图上。符号 ID 用于 layer.symbol。
+ * ## mapboxgl.Map.prototype.addSymbol
+ * 添加一个符号。该符号可以显示在地图上。Mapbox layers 的 symbol属性可以使用该符号ID。
  * 
- * 参数名称			     |类型			     |描述  
- * :----				|:---		        |:---	
- * id				    |string		        |符号ID
- * symbol			    |object	            |由[Mapbox Layers](https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/)中的paint、layout组成的符号对象
+ * |参数名称			     |类型			     |描述                | ||
+ * |----				|---		        |---			    |---|---|
+ * |id				    |string		        |符号ID              |||
+ * |symbol			    |object	            |由Mapbox Layers中的[paint](https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/#paint-property)、[layout](https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/#layout-property)组成的符号对象|||
+ * |                    |                   |参数名称			 |类型			     |描述  |
+ * |                    |                   |paint				|object		        |Mapbox Layers [paint](https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/#paint-property)|
+ * |                    |                   |layout			    |object	            |Mapbox Layers [layout](https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/#layout-property)|
  * 
+ * **Example**
  * ```
  * map.addSymbol('point-1', symbol);
  * ```
  * 
  * 
- * ## Map.setSymbol
+ * ## mapboxgl.Map.prototype.setSymbol
  * 给指定图层设置符号。
  * 
  * 参数名称			     |类型			 |描述
  * :----				|:---		    |:---	
  * layerId				|string		    |图层ID
- * symbolId			    |string		    |已经添加的符号ID
+ * symbolId			    |string		    |已经添加的符号ID（addSymbol中的符号ID)
  * 
+ * **Example**
  * ```
  * map.setSymbol("symbol", 'point-1');
  * ```
  * 
  * 
- * ## Map.hasSymbol
- * 检查样式中是否存在具有特定 ID 的符号.
+ * ## mapboxgl.Map.prototype.hasSymbol
+ * 检查是否存在特定 ID 的符号。
  * 
  * 参数名称			     |类型			 |描述  
  * :----			    |:---		    |:---	
  * symbolId			    |string		    |符号ID
  * 
+ * **Example**
  * ```
  * const pointExists = map.hasSymbol('point-1');
  * ```
  * 
  * 
- * ## Map.removeSymbol
- * 从样式中删除符号。
+ * ## mapboxgl.Map.prototype.removeSymbol
+ * 删除符号。
  * 
  * 参数名称			     |类型			 |描述  
  * :----				|:---		    |:---	
  * symbolId			    |string		    |已经添加的符号ID
  * 
+ * **Example**
  * ```
  * map.removeSymbol('point-1');
  * ```
  * 
  * 
- * ## 扩展 API
+ * ## 扩展 [Mapbox Layers](https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/)
+ * 在[Mapbox Layers](https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/) 属性的基础上新增了symbol 属性， 指定符号ID。
  * 
- * ## [Map.addLayer](https://docs.mapbox.com/mapbox-gl-js/api/map/#map#addlayer)
- * 添加图层。
- * 
- * 扩展参数名称			 |类型			 |描述  
- * :----				|:---		    |:---	
- * layer				|[Mapbox Layers](https://docs.mapbox.com/mapbox-gl-js/style-spec/layers/)		    |layer 图层, 新增了symbol属性
- * 
+ * **Example**
  * ```
  * map.addLayer({
  *     id: "symbol",
- *     source: "xx",
+ *     source: "sourceId",
  *     type: "symbol",
  *     symbol: 'point-1'
  * });
  * ```
- * 
- * 
- * ## [Map.setStyle](https://docs.mapbox.com/mapbox-gl-js/api/map/#map#setstyle)
- * 用新值更新地图的 Mapbox 样式对象。
- * 
- * 扩展参数名称			 |类型			           |描述  
- * :----				|:---		              |:---	
- * style				| [Mapbox Style](https://docs.mapbox.com/mapbox-gl-js/style-spec/)    |样式， 其中layer 图层, 新增了symbol属性
- * 
  * ```
  * map.setStyle({
  *     version: 8,
  *     sources: {},
  *     layers: [{
  *         id: "symbol",
- *         source: "xx",
+ *         source: "sourceId",
  *         type: "symbol",
  *         symbol: 'point-1'
  *     }]
  * })
  * ```
- * 
  */
